dc735772f6e83deabebc791ae4b829c6c966486a
Audrey Huang <auhhuang@milan2.cm.cluster>
2024-08-02 16:57:26 -0400

    feat: Add script to run GA on Seawulf

---
+++ scripts/run_ga.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=RUN_GA_100K
+#SBATCH --ntasks-per-node=96
+#SBATCH --nodes=1
+#SBATCH -p extended-96core
+#SBATCH --time=7-00:00:00
+#SBATCH --out=test-out.%j
+#SBATCH --error=test-err.%j
+cd $HOME
+source eif4e-inhibitor-discovery/.venv/bin/activate
+python eif4e-inhibitor-discovery/src/run_GA.py
